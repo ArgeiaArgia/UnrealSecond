6127e59c1ab81107bcc30be2f9f90bc7ac2e7cd5
djEjsdlfma <asdakpol.asd@gmail.com>
2026-09-21 09:41:38 +0900

    fix: waterbuildbug
    왠지는모르겠는데 생기는 버그 수정

--- Source/UnrealTeamProject/Gimmick/PuzzleWaterArea.cpp
+++ Source/UnrealTeamProject/Gimmick/PuzzleWaterArea.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Gimmick/PuzzleWaterArea.h"
+#include "PuzzleWaterArea.h"
 
 // Sets default values
 APuzzleWaterArea::APuzzleWaterArea()
